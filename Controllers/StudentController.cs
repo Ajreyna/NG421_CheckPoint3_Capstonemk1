d88490ad03e05f69aeb380612111c1974a0813db
Jon Woo <JWoo@eleadcrm.com>
2020-02-02 21:55:29 -0600

    init

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using capstone.Data;
+using capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace capstone.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public StudentController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            Student[] students = null;
+            using (var context = new ApplicationDbContext())
+            {
+                students =  context.Students.ToArray();
+            }
+            return students;
+            
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper.Configuration;
+using capstone.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Data
+{
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public IConfiguration Configuration { get; }
+        public DbSet<Student> Students { get; set; }
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+
+        }
+        public ApplicationDbContext() : base(new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite("Data Source=app.db").Options,null)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().HasData(
+            new Student { Id = 1, FirstName = "Jon", LastName = "Smith" },
+            new Student { Id = 2, FirstName = "Bobby", LastName = "Miller" },
+            new Student { Id = 3, FirstName = "Sarah", LastName = "Brooks" }
+            );
+        }
+    }
+}
