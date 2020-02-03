a4fd064f8a38fae833446b29f3d4e6203c3515b2
Jon Woo <JWoo@eleadcrm.com>
2020-02-03 09:50:45 -0600

    add solution

--- Startup.cs
+++ Startup.cs
@@ -50,13 +50,6 @@ namespace capstone
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                context.Database.Migrate();
-            }
-
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
